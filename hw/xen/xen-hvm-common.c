7c9b100a4dfd9dcfad4e6c0b2152aefa1c98e74f
Juergen Gross <jgross@suse.com>
2023-10-25 14:24:16 -0700

    xen: add pseudo RAM region for grant mappings
    Add a memory region which can be used to automatically map granted
    memory. It is starting at 0x8000000000000000ULL in order to be able to
    distinguish it from normal RAM.
    
    For this reason the xen.ram memory region is expanded, which has no
    further impact as it is used just as a container of the real RAM
    regions and now the grant region.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Vikram Garhwal <vikram.garhwal@amd.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

--- hw/xen/xen-hvm-common.c
+++ hw/xen/xen-hvm-common.c
@@ -9,7 +9,7 @@
 #include "hw/boards.h"
 #include "hw/xen/arch_hvm.h"
 
-MemoryRegion xen_memory;
+MemoryRegion xen_memory, ram_grants;
 
 void xen_ram_alloc(ram_addr_t ram_addr, ram_addr_t size, MemoryRegion *mr,
                    Error **errp)
@@ -26,7 +26,7 @@ void xen_ram_alloc(ram_addr_t ram_addr, ram_addr_t size, MemoryRegion *mr,
         return;
     }
 
-    if (mr == &xen_memory) {
+    if (mr == &xen_memory || mr == &ram_grants) {
         return;
     }
 
