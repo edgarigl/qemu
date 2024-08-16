902c809309607a57e11a11b56bc4c1f20a535bad
Edgar E. Iglesias <edgar.iglesias@amd.com>
2024-08-16 15:46:32 +0200

    hw/arm: xenpvh: Move stubbed functions to xen-stubs.c
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- hw/arm/xen_arm.c
+++ hw/arm/xen_arm.c
@@ -112,26 +112,6 @@ static void xen_init_ram(MachineState *machine)
     ram_grants = *xen_init_grant_ram();
 }
 
-void arch_handle_ioreq(XenIOState *state, ioreq_t *req)
-{
-    hw_error("Invalid ioreq type 0x%x\n", req->type);
-
-    return;
-}
-
-void arch_xen_set_memory(XenIOState *state, MemoryRegionSection *section,
-                         bool add)
-{
-}
-
-void xen_hvm_modified_memory(ram_addr_t start, ram_addr_t length)
-{
-}
-
-void qmp_xen_set_global_dirty_log(bool enable, Error **errp)
-{
-}
-
 #ifdef CONFIG_TPM
 static void xen_enable_tpm(XenArmState *xam)
 {
