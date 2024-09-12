8e7a18d8d7a985ff8cb14666d4af04c652b0fea1
Edgar E. Iglesias <edgar.iglesias@amd.com>
2024-09-12 21:57:51 +0200

    Revert "virtio-msg: Avoid using the IOMMU when possible"
    This reverts commit d9e5d3d12530c3f339978b8492431385db2db68d.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

--- hw/virtio/virtio-msg.c
+++ hw/virtio/virtio-msg.c
@@ -375,22 +375,7 @@ static AddressSpace *virtio_msg_get_dma_as(DeviceState *d)
 {
     VirtIOMSGProxy *s = VIRTIO_MSG(d);
 
-    /*
-     * Grant mappings don't work with IOMMU enabled but we really only need
-     * to enabled the iommu for virtio-msg across hypervisor instances where
-     * grant mappings don't make sense anyway.
-     *
-     * When iommu_enabled is on, performance drops significantly (~x10).
-     */
-    if (s->iommu_enabled) {
-        return &s->dma_as;
-    } else if (s->bus_as) {
-        /* If the bus provieds an AS, use it.  */
-        return s->bus_as;
-    } else {
-        /* Otherwise fall back to the default address space.  */
-        return &address_space_memory;
-    }
+    return &s->dma_as;
 }
 
 static Property virtio_msg_properties[] = {
