25ff80f4046f13bea96eff1eca2ebebb5658c3fd
Edgar E. Iglesias <edgar.iglesias@amd.com>
2025-09-23 08:36:44 +0200

    virtio-msg: Add a dedicated virtio-msg machine
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

---
+++ hw/virtio/virtio-msg-machine.c
@@ -0,0 +1,45 @@
+#include "qemu/osdep.h"
+
+#include "exec/memory.h"
+#include "qapi/error.h"
+#include "hw/qdev-core.h"
+#include "sysemu/dma.h"
+#include "hw/virtio/virtio-msg-machine.h"
+
+static void virtio_msg_machine_init(MachineState *machine)
+{
+    VirtIOMSGMachineState *s = VIRTIO_MSG_MACHINE(machine);
+    int i;
+
+    for (i = 0; i < ARRAY_SIZE(s->backends); i++) {
+        object_initialize_child(OBJECT(s), "backend[*]", &s->backends[i],
+                                TYPE_VIRTIO_MSG);
+        sysbus_realize(SYS_BUS_DEVICE(&s->backends[i]), &error_fatal);
+    }
+}
+
+static void virtio_msg_machine_class_init(ObjectClass *oc, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+
+    mc->init = virtio_msg_machine_init;
+    mc->desc = "Experimental virtio-msg machine";
+}
+
+static const TypeInfo virtio_msg_machine = {
+    .name = TYPE_VIRTIO_MSG_MACHINE,
+    .parent = TYPE_MACHINE,
+    .instance_size = sizeof(VirtIOMSGMachineState),
+    .class_init = virtio_msg_machine_class_init,
+    .interfaces = (InterfaceInfo[]) {
+        { TYPE_HOTPLUG_HANDLER },
+        { }
+    }
+};
+
+static void virtio_msg_machine_register_types(void)
+{
+    type_register_static(&virtio_msg_machine);
+}
+
+type_init(virtio_msg_machine_register_types);
---
+++ include/hw/virtio/virtio-msg-machine.h
@@ -0,0 +1,16 @@
+#ifndef HW_VIRTIO_VIRTIO_MSG_MACHINE_H
+#define HW_VIRTIO_VIRTIO_MSG_MACHINE_H
+
+#include "qom/object.h"
+#include "hw/boards.h"
+#include "hw/virtio/virtio-msg.h"
+
+struct VirtIOMSGMachineState {
+    MachineState parent_obj;
+
+    VirtIOMSGProxy backends[4];
+};
+
+#define TYPE_VIRTIO_MSG_MACHINE "x-virtio-msg-machine"
+OBJECT_DECLARE_SIMPLE_TYPE(VirtIOMSGMachineState, VIRTIO_MSG_MACHINE)
+#endif
