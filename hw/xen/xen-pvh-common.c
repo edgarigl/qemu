edgarigl/qemu

commit ef0f55d62d15c5b8d74f951aa02ba80bc12e4ccd

hw/arm: xenpvh: Enable virtio-msg
Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

hw/xen/xen-pvh-common.c
@@ -70,6 +70,19 @@ static void xen_set_irq(void *opaque, int irq, int level)
     }
 }
 
+static void xen_create_virtio_msg_devices(XenPVHMachineState *s)
+{
+    int i;
+
+    for (i = 0; i < ARRAY_SIZE(s->virtio_msg.backends); i++) {
+        object_initialize_child(OBJECT(s), "backend[*]",
+                                &s->virtio_msg.backends[i], TYPE_VIRTIO_MSG);
+
+        sysbus_realize(SYS_BUS_DEVICE(&s->virtio_msg.backends[i]),
+                       &error_fatal);
+    }
+}
+
 static void xen_create_virtio_mmio_devices(XenPVHMachineState *s)
 {
     int i;
@@ -199,6 +212,10 @@ static void xen_pvh_init(MachineState *ms)
 
     if (s->cfg.virtio_mmio_num) {
         xen_create_virtio_mmio_devices(s);
+    }
+
+    if (xpc->has_virtio_msg) {
+        xen_create_virtio_msg_devices(s);
     }
 
 #ifdef CONFIG_TPM

hw/arm/Kconfig
@@ -23,6 +23,7 @@ config ARM_VIRT
     select PLATFORM_BUS
     select SMBIOS
     select VIRTIO_MMIO
+    select VIRTIO_MSG
     select ACPI_PCI
     select MEM_DEVICE
     select DIMM

hw/arm/xen-pvh.c
@@ -86,6 +86,7 @@ static void xen_arm_machine_class_init(ObjectClass *oc, void *data)
     xpc->has_pci = true;
     xpc->has_tpm = true;
     xpc->has_virtio_mmio = true;
+    xpc->has_virtio_msg = true;
 
     xen_pvh_class_setup_common_props(xpc);
 }

include/hw/xen/xen-pvh-common.h
@@ -15,6 +15,8 @@
 #include "hw/xen/xen-hvm-common.h"
 #include "hw/pci-host/gpex.h"
 
+#include "hw/virtio/virtio-msg.h"
+
 #define TYPE_XEN_PVH_MACHINE MACHINE_TYPE_NAME("xen-pvh-base")
 OBJECT_DECLARE_TYPE(XenPVHMachineState, XenPVHMachineClass,
                     XEN_PVH_MACHINE)
@@ -53,6 +55,7 @@ struct XenPVHMachineClass {
     bool has_pci;
     bool has_tpm;
     bool has_virtio_mmio;
+    bool has_virtio_msg;
 };
 
 struct XenPVHMachineState {
@@ -71,6 +74,11 @@ struct XenPVHMachineState {
         MemoryRegion mmio_alias;
         MemoryRegion mmio_high_alias;
     } pci;
+
+    struct {
+        BusState bus;
+        VirtIOMSGProxy backends[16];
+    } virtio_msg;
 
     struct {
         MemMapEntry ram_low, ram_high;

commit 32f04a03b130cd1d6947f70aa13b168b2a7c0672

xen: pvh: Fix order of virtio-mmio devices
Fix the order of virtio-mmio devices to bring back
the command-line ordering from before virtio-msg
got merged.

Fixes: ef0f55d62d (hw/arm: xenpvh: Enable virtio-msg)
Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

hw/xen/xen-pvh-common.c
@@ -293,12 +293,13 @@ static void xen_pvh_init(MachineState *ms)
                        &xen_memory_listener,
                        s->cfg.mapcache);
 
-    if (s->cfg.virtio_mmio_num) {
-        xen_create_virtio_mmio_devices(s);
-    }
-
+    /* Register virtio-msg before virtio-mmio to keep original bus order. */
     if (xpc->has_virtio_msg) {
         xen_create_virtio_msg_devices(s);
+    }
+
+    if (s->cfg.virtio_mmio_num) {
+        xen_create_virtio_mmio_devices(s);
     }
 
 #ifdef CONFIG_TPM