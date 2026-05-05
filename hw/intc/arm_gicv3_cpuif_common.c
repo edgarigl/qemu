fefeaf361c9bba5003f278bef3315a306c5fbe58
Edgar E. Iglesias <edgar.iglesias@amd.com>
2026-05-05 17:11:22 +0200

    hw/intc: Add GICv3 FMU SError injection
    Add an experimental x-fmu virt machine property that exposes a minimal GIC-600AE-inspired FMU register window for qtest-driven SError injection.
    
    Implement the FMU_SMINJERR register as a physical SError source. Route delivery through the GICv3 CPU interface code so common GICv3 MMIO code does not depend directly on the ARM CPU SError helper.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

--- hw/intc/arm_gicv3_cpuif_common.c
+++ hw/intc/arm_gicv3_cpuif_common.c
@@ -11,6 +11,7 @@
 
 #include "qemu/osdep.h"
 #include "gicv3_internal.h"
+#include "hw/arm/arm-serror.h"
 #include "cpu.h"
 
 void gicv3_set_gicv3state(CPUState *cpu, GICv3CPUState *s)
@@ -20,3 +21,14 @@ void gicv3_set_gicv3state(CPUState *cpu, GICv3CPUState *s)
 
     env->gicv3state = (void *)s;
 };
+
+void gicv3_cpuif_set_serror(GICv3State *s, uint64_t esr, bool has_esr)
+{
+    CPUState *cs = current_cpu;
+
+    if (!cs && s->cpu) {
+        cs = s->cpu[0].cpu;
+    }
+
+    arm_cpu_set_serror(cs, esr, has_esr);
+}
